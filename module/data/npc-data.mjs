3e52abedbcf1ad2b7924ab89814d054bb5735008
GMonline-ua <gmonlineua@gmail.com>
2026-05-06 16:42:32 +0300

    feat: handle name and alias

--- module/data/npc-data.mjs
+++ module/data/npc-data.mjs
@@ -2,8 +2,11 @@ export default class NpcData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     const fields = foundry.data.fields;
     return {
-      realName: new fields.StringField(),
-      aka: new fields.StringField(),
+      names: new fields.SchemaField({
+        useAlias: new fields.BooleanField({ initial: false }),
+        real: new fields.StringField(),
+        alias: new fields.StringField(),
+      }),
       summary: new fields.StringField(),
       description: new fields.HTMLField(),
     };
