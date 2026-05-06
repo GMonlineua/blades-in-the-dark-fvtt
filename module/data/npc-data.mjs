9338ca100b1e43395ea4cea1dcd63033bb9cbfa8
GMonline-ua <gmonlineua@gmail.com>
2026-05-06 15:10:34 +0300

    feat: npcs translation & folders

--- module/data/npc-data.mjs
+++ module/data/npc-data.mjs
@@ -2,7 +2,7 @@ export default class NpcData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     const fields = foundry.data.fields;
     return {
-      name: new fields.StringField(),
+      realName: new fields.StringField(),
       aka: new fields.StringField(),
       summary: new fields.StringField(),
       description: new fields.HTMLField(),
